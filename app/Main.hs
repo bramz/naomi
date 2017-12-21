b6533d68174c781c1b7e97ad10afc45ec128d7b4
Forest Belton <forest@homolo.gy>
2017-12-20 21:40:59 -0800

    Add logging and fix deadlock

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
+import Control.Logging (withStdoutLogging)
 import Control.Concurrent.STM (newTChanIO)
 import Wuss (runSecureClient)
 
 import Naomi.Core (client)
 
 main :: IO ()
-main = do
+main = withStdoutLogging $ do
     chan <- newTChanIO
     runSecureClient "gateway.discord.gg" 443 "/" $ client chan
--- src/Naomi/Core.hs
+++ src/Naomi/Core.hs
@@ -1,19 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Naomi.Core (client) where
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM (atomically, TChan, newTChan, writeTChan, readTChan)
 import Control.Monad (forever, void)
+import qualified Control.Logging as L (debug, log)
+import Data.Text (pack)
 import Network.WebSockets (ClientApp, receiveData, sendTextData)
 
 import Naomi.Discord.Packet (Packet)
 
 client :: TChan Packet -> ClientApp ()
 client chan conn = do
-    putStrLn "Connected!"
+    L.log "Connected to Discord"
 
-    void . forkIO . forever $ writeTChan chan <$> receiveData conn
+    void . forkIO . forever $ do
+        msg <- receiveData conn
+        L.debug $ pack $ show msg
+        atomically $ writeTChan chan msg
 
-    let loop = do packet <- atomically $ readTChan chan
-                  print packet
-                  loop
-    loop
+    getChar >> return ()
